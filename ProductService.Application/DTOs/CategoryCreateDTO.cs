dfe7d8c2ee964e0904c05074115354001a64bb4e
Administrator <Administrator@SUJANHOME>
2025-08-13 21:39:25 +0600

    productService.application started

---
+++ ProductService.Application/DTOs/CategoryCreateDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Application.DTOs
+{
+    public class CategoryCreateDTO
+    {
+        [Required(ErrorMessage = "Category Name is required.")]
+
+        [StringLength(100, ErrorMessage = "Category Name cannot exceed 100 characters.")]
+
+        public string Name { get; set; } = null!;
+
+        [StringLength(500, ErrorMessage = "Category Description cannot exceed 500 characters.")]
+
+        public string? Description { get; set; }
+
+        public Guid? ParentCategoryId { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ProductService.Application/DTOs/CategoryUpdateDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Application.DTOs
+{
+    public class CategoryUpdateDTO
+    {
+        [Required(ErrorMessage = "Category Id is required.")]
+
+        public Guid Id { get; set; }
+
+
+
+        [Required(ErrorMessage = "Category Name is required.")]
+
+        [StringLength(100, ErrorMessage = "Category Name cannot exceed 100 characters.")]
+
+        public string Name { get; set; } = null!;
+
+
+
+        [StringLength(500, ErrorMessage = "Category Description cannot exceed 500 characters.")]
+
+        public string? Description { get; set; }
+
+        public Guid? ParentCategoryId { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
